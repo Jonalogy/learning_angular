0d42d7be4a723ea242caaa4c0b7385605c5d900a
jonalogy <jonalogy@gmail.com>
2017-08-06 22:27:44 +0800

    Implemented first FormGroup

---
+++ Forms/reactive-forms/src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.css']
+})
+export class HeroDetailComponent {
+  heroForm = new FormGroup({
+    name: new FormControl()
+  });
+}
